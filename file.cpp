1285b9d9c758d8b40d1e137e4a98a297bbe8880b
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-15 23:37:57 +0530

    Create file.cpp

---
+++ file.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+#include<fstream>
+int main()
+{
+	ofstream fout;
+	fout.open("country");
+	
+	fout<<"United States Of America\n";
+	fout<<"\n United Kingdom";
+	fout<<"\n South Korea";
+	
+	fout.close();
+	
+	fout.open("capital");
+	fout<<"Washington\n";
+	fout<<"\n London";
+	fout<<"\n Seoul";
+	
+    fout.close();
+    
+    const int N=80;
+    char line[N];
+    
+    ifstream fin;
+    fin.open("country");
+    
+    cout<<"contents of country:\n";
+    
+    while(fin)
+    {
+    	fin.getline(line,N);
+    	cout<<line;
+    	
+	}
+	
+	fin.close();
+	fin.open("capital");
+	
+	cout<<"\n Contents of capital file";
+	
+	while(fin)
+	{
+		fin.getline(line,N);
+		cout<<line;
+		
+		
+	}
+	fin.close();
+	return 0;
+	
+}
