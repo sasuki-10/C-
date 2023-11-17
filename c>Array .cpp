2d54ba9d8f0e573038e26506d400a58afb0a9f78
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-17 22:46:05 +0530

    c> Array .cpp

---
+++ c>Array .cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a[100],i;
+  int n,sum=0;
+    
+	 cout<<"enter the number:";
+     cin>>n;
+         
+		 if(n<100)
+          {
+          	for(i=0;i<=n;i++)
+            { 
+            cout<<"a[%d]= "<<i;
+            cin>>a[i];
+            sum=sum+i;
+            }
+        }
+      else
+          {
+          cout<<"out of array";
+           }
+    
+   cout<<"sum is:"<<sum<<endl;
+     return 0;
+}
