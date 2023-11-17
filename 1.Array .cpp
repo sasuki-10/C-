f908a311afb83e1434105d5f6d0f2fc4708d472f
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-17 22:42:26 +0530

    1.Array .cpp

--- 1.Array .cpp
+++ 1.Array .cpp
@@ -3,10 +3,12 @@ using namespace std;
 int main()
 {
     int a[10],i;  // Here 10 is size of array.....
+    
     for(i=0;i<=9;i++)
     {
     	cout<<"INSERT ELEMENT:"
         cin >>a[i];  
     }
+   
     return 0;
 }  
