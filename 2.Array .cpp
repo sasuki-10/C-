cd5b6706006c789de133b4f5f7d39b53a36e315b
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-17 22:43:18 +0530

    2.Array .cpp

---
+++ 2.Array .cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a[100],i,n;
+  cout<<"enter the number:";
+  cin>>n;
+     
+           if(n<100)
+          {
+            cin>>a[i];
+          }
+      else
+          {
+           cout<<"out of array";
+           }
+    }
+     return 0;
+}
