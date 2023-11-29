f0fdf3aa6a7b3090d808e0eb90ee1c8366a7e8a5
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-11-29 23:29:55 +0530

    pointer.cpp

---
+++ pointer.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	int a,*ptr1,**ptr2;
+	clrscr();
+	ptr1=&a;
+	ptr2=&ptr1;
+	
+	cout<<"THE ADDRESS OF a:"<<ptr1<<"\n";
+	cout<<"THE ADDRESS OF ptr1:"<<ptr2;
+	cout<<"\n\n";
+	cout<<"AFTER INCREMENTING THE ADDRESS VALUE:\n\n";
+	ptr1+=2;
+	cout<<"THE ADDRESS OF a:"<<ptr1<<"\n";
+	ptr2+=2;
+	cout<<"THE ADDRESS OF ptr1:"<<ptr2<<"\n";
+}
