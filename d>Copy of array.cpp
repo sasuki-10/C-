3860523d6207437b345abb8a56e2415780e46737
TRIPTI SHARMA <128908454+sasuki-10@users.noreply.github.com>
2023-12-05 23:37:29 +0530

    d>Copy of array.cpp

--- d>Copy of array.cpp
+++ d>Copy of array.cpp
@@ -6,15 +6,16 @@ int main()
  int arr2[5];
  int i;
  cout<<"array elements are:";
- for(i=0;i<5;i++)
-    {
- 	    cin>>arr1[i];
+    for(i=0;i<5;i++)
+      {
+ 	        cin>>arr1[i];
  		arr2[i]=arr1[i];
-	 }	
+      }	
 
 	cout<<"\n/*******DISPLAYING***********/";
+	
 	for(i=0;i<5;i++)
-    {
+       {
  	cout<<arr2[i]<<endl;
-}
+       }
 }
